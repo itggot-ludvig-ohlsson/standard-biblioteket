695e6bd05d6fa92780dfac4142cea305d123384c
itggot-ludvig-ohlsson <38528638+itggot-ludvig-ohlsson@users.noreply.github.com>
2018-04-19 15:06:58 +0200

    Created max_of_three.rb

---
+++ lib/max_of_three.rb
@@ -0,0 +1,25 @@
+def max_of_three(n1, n2, n3)
+    if n1 > n2 && n1 > n3
+        return n1
+    elsif n2 > n3 && n2 > n1
+        return n2
+    else
+        return n3
+    end
+
+=begin # Alternativ implementation, kanske är snabbare
+    if n1 > n2
+        if n1 > n3
+            return n1
+        else
+            return n3
+        end
+    else
+        if n2 > n3
+            return n2
+        else
+            return n3
+        end
+    end
+=end
+end
