76505a337fb72d0883a8bdc9a60357a05271014a
itggot-ludvig-ohlsson <38528638+itggot-ludvig-ohlsson@users.noreply.github.com>
2018-04-26 15:00:34 +0200

    Created first_of.rb

---
+++ lib/first_of.rb
@@ -0,0 +1,16 @@
+# Public: Get the first item in an array.
+#
+# arr  - The Array to get the item from.
+#
+# Examples
+#
+#   sum_to(3)
+#   # => 6
+#   
+#   sum_to(5)
+#   # => 15
+#
+# Returns the first item in the Array.
+def first_of(arr)
+    return arr[0]
+end
