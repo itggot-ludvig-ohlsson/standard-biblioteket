0888a9721c081edc9d009b98943c2c4bb4f09124
itggot-ludvig-ohlsson <38528638+itggot-ludvig-ohlsson@users.noreply.github.com>
2018-05-03 13:19:07 +0200

    Created append.rb

---
+++ lib/append.rb
@@ -0,0 +1,14 @@
+# Public: Append a number to an array.
+#
+# arr  - The Array to be appended to.
+# num  - The Integer to be appended.
+#
+# Examples
+#
+#   append([1, 2, 3], 4)
+#   # => [1, 2, 3, 4]
+#
+# Returns the sum of all numbers from 0 to the Integer.
+def append(arr, num)
+    return arr << num
+end
