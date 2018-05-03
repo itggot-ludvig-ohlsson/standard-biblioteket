1d74f07fe2307f32186e3757bc353cdf5a09fc29
itggot-ludvig-ohlsson <38528638+itggot-ludvig-ohlsson@users.noreply.github.com>
2018-05-03 13:29:23 +0200

    Created sum.rb

---
+++ lib/sum.rb
@@ -0,0 +1,21 @@
+# Public: Sum all of the integers in an array.
+#
+# arr  - The Array containing the integers to be summed.
+#
+# Examples
+#
+#   puts sum([1, 2, 3, 4, 5])
+#   # => 15
+#
+# Returns the sum of all the integers in the array.
+def sum(arr)
+    i = 0
+    total = 0
+    while i < arr.length
+        total += arr[i]
+
+        i += 1
+    end
+
+    return total
+end
