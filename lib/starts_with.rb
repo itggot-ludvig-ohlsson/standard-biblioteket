3016de51e31e134feb03eb0954db96c091c918ec
itggot-ludvig-ohlsson <38528638+itggot-ludvig-ohlsson@users.noreply.github.com>
2018-05-03 13:43:10 +0200

    Created starts_with.rb

---
+++ lib/starts_with.rb
@@ -0,0 +1,17 @@
+# Public: Checks if a string starts with a certain character.
+#
+# str  - The String to be checked.
+# char - The character to check if the string starts with
+#
+# Examples
+#
+#   starts_with("hej hopp", "h")
+#   # => true
+#
+#   start_with("Hello World", "k")
+#   # => false
+#
+# Returns if the string starts with the character or not.
+def starts_with(str, char)
+    return str[0] == char
+end
